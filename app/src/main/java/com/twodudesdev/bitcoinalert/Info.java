e5ff1ecce4769c668638551fa4e427a2554b1acb
Jimmy James <that.one.guy.on.the.street@gmail.com>
2018-03-26 21:07:27 -0700

    This is where we ran into the Get Issue on MainActivity.java

---
+++ app/src/main/java/com/twodudesdev/bitcoinalert/Info.java
@@ -0,0 +1,15 @@
+package com.twodudesdev.bitcoinalert;
+
+/**
+ * Created by Sid and totally not Will on 3/26/2018.
+ */
+
+public class Info {
+    public int id;
+    public String mid;
+    public String bid;
+    public String ask;
+    public String last_price;
+    public String timestamp;
+
+}
--- app/src/main/java/com/twodudesdev/bitcoinalert/MainActivity.java
+++ app/src/main/java/com/twodudesdev/bitcoinalert/MainActivity.java
@@ -2,12 +2,31 @@ package com.twodudesdev.bitcoinalert;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import android.database.sqlite.SQLiteDatabase;
+import android.widget.Toast;
+import com.loopj.android.http.*;
+import cz.msebera.android.httpclient.Header;
+
+
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    AsyncHttpClient client = new AsyncHttpClient();
+    client.get("https://api.bitfinex.com/v1/ticker/btcusd", new TextHttpResponseHandler(){
+        @Override
+                public void onFailure(int statusCode, Header[] headers, String response, Throwable throwable){
+            Toast.makeText(this, "Cannot load JSON File", Toast.LENGTH_SHORT).show();
+
+        }
+
+    }
+
 }
