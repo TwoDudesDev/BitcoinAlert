3a7855805086e5712381d81858f6267f884671b2
Will Hughes <generilisk@gmail.com>
2018-03-28 11:55:06 -0700

    Fixed the client.get error, added onSuccess method, added internet perms

--- app/src/main/java/com/twodudesdev/bitcoinalert/MainActivity.java
+++ app/src/main/java/com/twodudesdev/bitcoinalert/MainActivity.java
@@ -1,35 +1,40 @@
 package com.twodudesdev.bitcoinalert;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import android.database.sqlite.SQLiteDatabase;
 import android.widget.Toast;
-import com.loopj.android.http.*;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.TextHttpResponseHandler;
 import cz.msebera.android.httpclient.Header;
 
 
 
 public class MainActivity extends AppCompatActivity {
+    Context context = this;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get("https://api.bitfinex.com/v1/ticker/btcusd", new TextHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, String response) {
+                String toastText = "Successfully downloaded JSON File";
+                Toast successfulJsonToast = Toast.makeText(context, toastText, Toast.LENGTH_SHORT);
+                successfulJsonToast.show();
+            }
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String response, Throwable throwable) {
+                String toastText = "Cannot load JSON File: " + throwable;
+                Toast failedJsonToast = Toast.makeText(context, toastText, Toast.LENGTH_SHORT);
+                failedJsonToast.show();
+            }
+        });
     }
-    AsyncHttpClient client = new AsyncHttpClient();
-    client.get("https://api.bitfinex.com/v1/ticker/btcusd", new TextHttpResponseHandler(){
-        @Override
-                public void onFailure(int statusCode, Header[] headers, String response, Throwable throwable){
-            Toast.makeText(this, "Cannot load JSON File", Toast.LENGTH_SHORT).show();
-            /* It wouldn't let me save until I changed something so, bad line of code here;
-            turned the bad line into a comment
-             */
-
-        }
-
-    }
-
 }
