1eb42133ae517657d60395589ce0c45fd914c05c
Jimmy James <that.one.guy.on.the.street@gmail.com>
2018-04-23 21:02:37 -0700

    Finished the 2 minute timer for the display text

--- app/src/main/java/com/twodudesdev/bitcoinalert/MainActivity.java
+++ app/src/main/java/com/twodudesdev/bitcoinalert/MainActivity.java
@@ -1,20 +1,38 @@
 package com.twodudesdev.bitcoinalert;
 
+import android.app.Notification;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.nfc.Tag;
+import android.support.annotation.NonNull;
+import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+
+import com.evernote.android.job.Job;
+import com.evernote.android.job.JobCreator;
+import com.evernote.android.job.JobManager;
+import com.evernote.android.job.JobRequest;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import android.database.sqlite.SQLiteDatabase;
 import android.widget.Toast;
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.TextHttpResponseHandler;
+
+import java.util.Random;
+
 import cz.msebera.android.httpclient.Header;
 
 
 
 public class MainActivity extends AppCompatActivity {
     Context context = this;
+    String Burger = "yes";
+
 
 
     @Override
@@ -22,6 +40,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         AsyncHttpClient client = new AsyncHttpClient();
+        dataGet(client);
+        JobManager.create(this).addJobCreator(new DemoJobCreator());
+    }
+    private void dataGet(AsyncHttpClient client) {
         client.get("https://api.bitfinex.com/v1/ticker/btcusd", new TextHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Header[] headers, String response) {
@@ -37,4 +59,54 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+
+
 }
+
+class downloadAndSaveData extends Job {
+    static final String TAG = "Download_Current_Price";
+
+
+    @NonNull
+    @Override
+    protected Result onRunJob(Params params){
+        String ted = "stuff";
+        PendingIntent pi = PendingIntent.getActivity(getContext(),0, new Intent(getContext(), MainActivity.class),0);
+
+        NotificationCompat.Builder notification = new NotificationCompat.Builder(getContext(),ted)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle("Android Job Demo")
+                .setContentText("Notification from Android Job Demo App.")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        NotificationManagerCompat jobNoticeManager = NotificationManagerCompat.from(getContext());
+        jobNoticeManager.notify(7, notification.build());
+
+
+        return Result.SUCCESS;
+        }
+
+        static void schedulePeriodic(){
+        new JobRequest.Builder(downloadAndSaveData.TAG)
+                .setPeriodic(120000)
+                .setUpdateCurrent(true)
+                .setPersisted(true)
+                .build()
+                .schedule();
+        }
+}
+
+
+class DemoJobCreator implements JobCreator{
+    @Override
+    public Job create(String tag){
+        switch (tag){
+            case downloadAndSaveData.TAG:
+                return new downloadAndSaveData();
+            default:
+                return null;
+        }
+    }
+}
+
